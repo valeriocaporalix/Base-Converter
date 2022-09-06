61d8bde3ff5b39c87004d6badab57034798b07aa
Valerio Caporali <cyndoriil@gmail.com>
2022-09-06 16:56:36 +0200

    Form Card Implementation

---
+++ src/components/FormCard.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import BaseConverter from '../utility/BaseConverter';
+
+const FormCard = () => {
+  const [num, setNum] = useState('');
+  const [fromBase, setFromBase] = useState(10);
+  const [toBase, setToBase] = useState(16);
+  const [result, setResult] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setResult(BaseConverter(fromBase, num, toBase));
+  };
+  return (
+    <section>
+      <form onSubmit={submitHandler}>
+        <label>Number:</label>
+        <input value={num} onChange={(e) => setNum(e.target.value)} required />
+        <label>From Base:</label>
+        <select onChange={(e) => setFromBase(e.target.value)} defaultValue={10}>
+          <option value={2}>{'2 (Binary)'}</option>
+          <option value={3}>{'3'}</option>
+          <option value={4}>{'4'}</option>
+          <option value={5}>{'5'}</option>
+          <option value={6}>{'6'}</option>
+          <option value={7}>{'7'}</option>
+          <option value={8}>{'8 (Octal)'}</option>
+          <option value={9}>{'9'}</option>
+          <option value={10}>{'10 (Decimal)'}</option>
+          <option value={11}>{'11'}</option>
+          <option value={12}>{'12'}</option>
+          <option value={13}>{'13'}</option>
+          <option value={14}>{'14'}</option>
+          <option value={15}>{'15'}</option>
+          <option value={16}>{'16 (Hex)'}</option>
+        </select>
+        <label>To Base:</label>
+        <select onChange={(e) => setToBase(e.target.value)} defaultValue={16}>
+          <option value={2}>{'2 (Binary)'}</option>
+          <option value={3}>{'3'}</option>
+          <option value={4}>{'4'}</option>
+          <option value={5}>{'5'}</option>
+          <option value={6}>{'6'}</option>
+          <option value={7}>{'7'}</option>
+          <option value={8}>{'8 (Octal)'}</option>
+          <option value={9}>{'9'}</option>
+          <option value={10}>{'10 (Decimal)'}</option>
+          <option value={11}>{'11'}</option>
+          <option value={12}>{'12'}</option>
+          <option value={13}>{'13'}</option>
+          <option value={14}>{'14'}</option>
+          <option value={15}>{'15'}</option>
+          <option value={16}>{'16 (Hex)'}</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+      <div className="result">
+        <label>Result:</label>
+        <div className="result-container">
+          <h4>{result === '' ? '...' : result}</h4>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FormCard;
